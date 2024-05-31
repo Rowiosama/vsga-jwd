5bea3600a382284a6eb2c05d372e31193e1d42aa
Muhammad Rowi Osama <rowiosama13@gmail.com>
2024-05-31 19:54:03 +0700

    tugas dan materi

---
+++ tugas/tugas_satu.php
@@ -0,0 +1,7 @@
+<?php
+echo "Nama : Muhammad Rowi Osama <br>"
+  . "Asal Kampus : Universitas Islam Negeri Raden Intan Lampung <br>"
+  . "Jurusan : Sistem Informasi <br>"
+  . "Kota : Bandar Lampung <br>"
+  . "PHP : Pernah <br>"
+  . "HTML : Pernah <br>";
